dafedb32f5460ac713667dcd5cd7671ef5b91f83
staticGuru <vigneshguru274@gmail.com>
2024-07-01 17:02:57 +0530

    Add the custom input components

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 type InputProps = {
   type: string;
@@ -16,5 +16,24 @@ export function Input({
   onChange = () => {},
   shadow = false,
 }: InputProps) {
-  return <div>Input</div>;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+  return (
+    <input
+      ref={inputRef}
+      type={type}
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      className={`border p-2 w-full rounded md:rounded-md outline-none ${
+        shadow ? "shadow" : ""
+      } text-xs  md:text-sm `}
+    />
+  );
 }
