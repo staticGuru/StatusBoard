d198ca182b3a7b8af571fd9b54f4f5e9467f20ff
staticGuru <vigneshguru274@gmail.com>
2024-07-01 16:59:47 +0530

    check the modal secitons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import "./App.css";
+import { Button, Modal } from "./components/intex";
 
 function App() {
-  return <div className="mx-auto max-w-7xl">test</div>;
+  const [showAddContainerModal, setShowAddContainerModal] = useState(false);
+
+  return (
+    <div className="mx-auto max-w-7xl">
+       <Modal
+        showModal={showAddContainerModal}
+        setShowModal={setShowAddContainerModal}
+      >
+        <div className="flex flex-col w-full items-start gap-y-4">
+          <h1 className="text-gray-800 text-xl md:text-2xl font-bold text-center mx-auto">
+            Add Container
+          </h1>
+      
+        </div>
+      </Modal>
+      <div className="flex items-center justify-between gap-y-2">
+        <h1 className="text-gray-800 text-xl md:text-3xl font-bold">
+          StatusBoard
+        </h1>
+        <Button
+          onClick={() => setShowAddContainerModal(true)}
+          label="Add a new status board"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
