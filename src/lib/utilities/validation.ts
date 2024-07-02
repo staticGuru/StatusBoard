e5aa49ba37eae4f61abbfa130899fe00242b88c7
staticGuru <vigneshguru274@gmail.com>
2024-07-02 11:31:01 +0530

    check the editcontainer name change

--- src/lib/utilities/validation.ts
+++ src/lib/utilities/validation.ts
@@ -1,3 +1,6 @@
+import { UniqueIdentifier } from "@dnd-kit/core";
+import { ContainerType } from "..";
+
 export const isContainerNameEmpty = (containerName: string): boolean => {
   return containerName.trim() === "";
 };
@@ -5,3 +8,19 @@ export const isContainerNameEmpty = (containerName: string): boolean => {
 export const isItemNameEmpty = (itemName: string): boolean => {
   return itemName.trim() === "";
 };
+
+export const isEditingContainerNameChanged = (
+    editingContainerName: string,
+    editingContainer: UniqueIdentifier | null,
+    containers: ContainerType[]
+  ): boolean => {
+    return (
+      editingContainerName.trim() !== "" &&
+      containers.some(
+        (container) =>
+          container.id === editingContainer &&
+          container.title !== editingContainerName.trim()
+      )
+    );
+  };
+  
