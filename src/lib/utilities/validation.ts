ae1585dec444fe7187572ded28a77f382d745a03
staticGuru <vigneshguru274@gmail.com>
2024-07-02 11:30:20 +0530

    check the item name empty sections

--- src/lib/utilities/validation.ts
+++ src/lib/utilities/validation.ts
@@ -1,5 +1,7 @@
-
 export const isContainerNameEmpty = (containerName: string): boolean => {
   return containerName.trim() === "";
 };
 
+export const isItemNameEmpty = (itemName: string): boolean => {
+  return itemName.trim() === "";
+};
