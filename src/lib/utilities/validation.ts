c388ea2d0ed014ff3eebf595e6e16a4f1c2a9158
staticGuru <vigneshguru274@gmail.com>
2024-07-02 11:31:34 +0530

    add the validation for editing item name change

--- src/lib/utilities/validation.ts
+++ src/lib/utilities/validation.ts
@@ -10,17 +10,32 @@ export const isItemNameEmpty = (itemName: string): boolean => {
 };
 
 export const isEditingContainerNameChanged = (
-    editingContainerName: string,
-    editingContainer: UniqueIdentifier | null,
-    containers: ContainerType[]
-  ): boolean => {
-    return (
-      editingContainerName.trim() !== "" &&
-      containers.some(
-        (container) =>
-          container.id === editingContainer &&
-          container.title !== editingContainerName.trim()
+  editingContainerName: string,
+  editingContainer: UniqueIdentifier | null,
+  containers: ContainerType[]
+): boolean => {
+  return (
+    editingContainerName.trim() !== "" &&
+    containers.some(
+      (container) =>
+        container.id === editingContainer &&
+        container.title !== editingContainerName.trim()
+    )
+  );
+};
+
+export const isEditingItemNameChanged = (
+  editingItemName: string,
+  editingItem: UniqueIdentifier | null,
+  containers: ContainerType[]
+): boolean => {
+  return (
+    editingItemName.trim() !== "" &&
+    containers.some((container) =>
+      container.items.some(
+        (item) =>
+          item.id === editingItem && item.title !== editingItemName.trim()
       )
-    );
-  };
-  
+    )
+  );
+};
