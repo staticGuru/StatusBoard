a8c7f0c8e51d963ad76ed317d85a2c7c737d3d33
staticGuru <vigneshguru274@gmail.com>
2024-07-02 12:26:37 +0530

    export from index components

--- src/components/intex.ts
+++ src/components/intex.ts
@@ -2,5 +2,7 @@ import { lazy } from "react";
 
 export * from "./button/button";
 export * from "./input/input";
+export * from "./item/item";
+export * from "./container/container";
 
 export const Modal = lazy(() => import("./modal/modal"));
